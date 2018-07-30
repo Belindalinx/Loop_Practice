9f6ef24bfb5116b647bb131383bee3d5f7187d7a
belindalin <zercanopic@gmail.com>
2018-07-29 17:07:38 -0700

    while loop practice

---
+++ flip＿coin/flip coin.py
@@ -0,0 +1,24 @@
+#import random function
+import random
+
+#define variable
+coin=random.randint(1,2)
+times=0
+head=0
+tail=0
+
+#flipcoin
+while times<100:
+    coin=random.randint(1,2)
+    times+=1
+    if coin==1:
+        head+=1
+        #print(head)
+    else:
+        tail+=1
+        #print(tail)
+
+print("head",head,"times")
+print("tail",tail,"times")
+print("coin flipped ",times,"times")
+
